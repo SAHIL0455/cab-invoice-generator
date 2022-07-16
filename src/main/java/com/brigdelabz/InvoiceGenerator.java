a583e227e092cf6d43cfd13bcb861284cea7fc21
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-16 22:22:52 +0530

    ADDED: Total number of rides,Total Fare, Average Fare Per Ride

--- src/main/java/com/brigdelabz/InvoiceGenerator.java
+++ src/main/java/com/brigdelabz/InvoiceGenerator.java
@@ -4,6 +4,8 @@ public class InvoiceGenerator {
     public static double CostOfPerKilometer = 10;
     public static double PerMinRs = 1;
     public static double TotalRs = 0;
+    public static double TotalRide = 0;
+    public static double TotalAverageFare = 0;
 
     public double calculateFare(int distance, int time) {
         return distance * CostOfPerKilometer + time * PerMinRs;
@@ -13,6 +15,8 @@ public class InvoiceGenerator {
         for (Ride rides : ride) {
             TotalRs += calculateFare(rides.distance, rides.time);
         }
+        TotalRide = ride.length;
+        TotalAverageFare = TotalRs / TotalRide;
         return TotalRs;
     }
 }
--- src/test/java/com/brigdelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/brigdelabz/InvoiceGeneratorTest.java
@@ -19,10 +19,16 @@ public class InvoiceGeneratorTest {
     }
 
     @Test
-    public void givenMultiplyDistanceAndTime_shouldReturnTotalFare() {
+    public void givenMultiplyDistanceAndTime_shouldReturnTotalFare_TotalRide_TotalAverageFare() {
         Ride[] multiplyRides = {new Ride(5, 10),
                 new Ride(6, 12)};
         double actualResult = invoiceGenerator.calculateMultiplyFare(multiplyRides);
-        Assert.assertEquals(132, actualResult, 0.0);
+        Assert.assertEquals(132.0, actualResult, 0.0);
+
+        double actualResult1 = invoiceGenerator.TotalRide;
+        Assert.assertEquals(2, actualResult1, 0.0);
+
+        double actualResult2 = invoiceGenerator.TotalAverageFare;
+        Assert.assertEquals(66, actualResult2, 0.0);
     }
 }
